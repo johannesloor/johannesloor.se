f4a1dd66866a4d7ef80b73e67cb42c4a2f49e5a6
JOhohannes Loor <johannes.loor@gmail.com>
2020-05-04 11:55:38 +0200

    Removed unused import

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import { breakpoints } from "../styles/variables";
-
 import { widths } from "../styles/variables";
 import { getEmSize } from "../styles/mixins";
 
