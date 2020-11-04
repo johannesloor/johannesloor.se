7a105d558654a230a43f4cbe9720c89b19bf58cf
Johannes Loor <johannes.loor@gmail.com>
2020-11-04 16:39:03 +0100

    Updated 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,16 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import * as React from "react";
 
-import Page from '../components/Page'
-import Container from '../components/Container'
-import IndexLayout from '../layouts'
+import Page from "../components/Page";
+import IndexLayout from "../layouts";
+import PageTitle from "../components/PageTitle";
 
 const NotFoundPage = () => (
   <IndexLayout>
     <Page>
-      <Container>
-        <h1>404: Page not found.</h1>
-        <p>
-          You've hit the void. <Link to="/">Go back.</Link>
-        </p>
-      </Container>
+      <PageTitle>404: Page not found.</PageTitle>
+      <h3>Well this is awkward... Better click the clock.</h3>
     </Page>
   </IndexLayout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
